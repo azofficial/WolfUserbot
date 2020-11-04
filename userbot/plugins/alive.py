625c6049cac8ed4c582fc728fab3c8c91648172d
azofficial <62930663+azofficial@users.noreply.github.com>
2020-11-04 18:28:29 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,319 +1,81 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-"""Userbot module for getting information about the server."""
-import asyncio
-import platform
-import sys
-import time
-from asyncio import create_subprocess_exec as asyncrunapp
-from asyncio.subprocess import PIPE as asyncPIPE
-from datetime import datetime
-from os import remove
+"""Check if userbot alive or not . """
+
+
+import asyncio , time
+from telethon import events
+from userbot import StartTime
+from platform import uname
+from userbot import CMD_HELP, ALIVE_NAME, wolfversion , wolfdef
+from userbot.utils import admin_cmd,sudo_cmd
+from telethon import version
 from platform import python_version, uname
-from shutil import which
+import requests
+import re
+from PIL import Image
+import os
+import nekos
 
-import psutil
-from telethon import __version__, version
-from telethon.errors.rpcerrorlist import MediaEmptyError
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "wolf"
+WOLF_IMG = Config.ALIVE_PIC
 
-from userbot import (
-    ALIVE_LOGO,
-    ALIVE_NAME,
-    CMD_HELP,
-    PROJECTDILS_VERSION,
-    StartTime,
-    bot,
-)
-from userbot.events import register
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-modules = CMD_HELP
-# ============================================
-
-
-async def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    up_time += ":".join(time_list)
-
-    return up_time
-
-
-@register(outgoing=True, pattern=r"^\.spc")
-async def psu(event):
-    uname = platform.uname()
-    softw = "**System Information**\n"
-    softw += f"`System   : {uname.system}`\n"
-    softw += f"`Release  : {uname.release}`\n"
-    softw += f"`Version  : {uname.version}`\n"
-    softw += f"`Machine  : {uname.machine}`\n"
-    # Boot Time
-    boot_time_timestamp = psutil.boot_time()
-    bt = datetime.fromtimestamp(boot_time_timestamp)
-    softw += f"`Boot Time: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
-    # CPU Cores
-    cpuu = "**CPU Info**\n"
-    cpuu += "`Physical cores   : " + str(psutil.cpu_count(logical=False)) + "`\n"
-    cpuu += "`Total cores      : " + str(psutil.cpu_count(logical=True)) + "`\n"
-    # CPU frequencies
-    cpufreq = psutil.cpu_freq()
-    cpuu += f"`Max Frequency    : {cpufreq.max:.2f}Mhz`\n"
-    cpuu += f"`Min Frequency    : {cpufreq.min:.2f}Mhz`\n"
-    cpuu += f"`Current Frequency: {cpufreq.current:.2f}Mhz`\n\n"
-    # CPU usage
-    cpuu += "**CPU Usage Per Core**\n"
-    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
-        cpuu += f"`Core {i}  : {percentage}%`\n"
-    cpuu += "**Total CPU Usage**\n"
-    cpuu += f"`All Core: {psutil.cpu_percent()}%`\n"
-    # RAM Usage
-    svmem = psutil.virtual_memory()
-    memm = "**Memory Usage**\n"
-    memm += f"`Total     : {get_size(svmem.total)}`\n"
-    memm += f"`Available : {get_size(svmem.available)}`\n"
-    memm += f"`Used      : {get_size(svmem.used)}`\n"
-    memm += f"`Percentage: {svmem.percent}%`\n"
-    # Bandwidth Usage
-    bw = "**Bandwith Usage**\n"
-    bw += f"`Upload  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
-    bw += f"`Download: {get_size(psutil.net_io_counters().bytes_recv)}`\n"
-    help_string = f"{str(softw)}\n"
-    help_string += f"{str(cpuu)}\n"
-    help_string += f"{str(memm)}\n"
-    help_string += f"{str(bw)}\n"
-    help_string += "**Engine Info**\n"
-    help_string += f"`Python {sys.version}`\n"
-    help_string += f"`Telethon {__version__}`"
-    await event.edit(help_string)
-
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
-
-
-@register(outgoing=True, pattern=r"^\.sysd$")
-async def sysdetails(sysd):
-    """For .sysd command, get system info using neofetch."""
-    if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
-        try:
-            fetch = await asyncrunapp(
-                "neofetch",
-                "--stdout",
-                stdout=asyncPIPE,
-                stderr=asyncPIPE,
-            )
-
-            stdout, stderr = await fetch.communicate()
-            result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-            await sysd.edit("`" + result + "`")
-        except FileNotFoundError:
-            await sysd.edit("`Install neofetch first !!`")
-
-
-@register(outgoing=True, pattern=r"^\.botver$")
-async def bot_ver(event):
-    """For .botver command, get the bot version."""
-    if event.text[0].isalpha() or event.text[0] in ("/", "#", "@", "!"):
-        return
-    if which("git") is not None:
-        ver = await asyncrunapp(
-            "git",
-            "describe",
-            "--all",
-            "--long",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await ver.communicate()
-        verout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        rev = await asyncrunapp(
-            "git",
-            "rev-list",
-            "--all",
-            "--count",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await rev.communicate()
-        revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        com = await asyncrunapp(
-            "git",
-            "log",
-            "--pretty='%h : %s'",
-            "-1",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await com.communicate()
-        comout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        await event.edit(
-            f"`Userbot Version: {verout}`\n"
-            f"`Revision: {revout}`\n"
-            f"`Latest commit: {comout}`"
-        )
-    else:
-        await event.edit(
-            "Shame that you don't have git, you're running - 'v1.beta.4' anyway!"
-        )
-
-
-@register(outgoing=True, pattern=r"^\.pip(?: |$)(.*)")
-async def pipcheck(pip):
-    """For .pip command, do a pip search."""
-    if pip.text[0].isalpha() or pip.text[0] in ("/", "#", "@", "!"):
-        return
-    pipmodule = pip.pattern_match.group(1)
-    if pipmodule:
-        await pip.edit("`Searching . . .`")
-        pipc = await asyncrunapp(
-            "pip3",
-            "search",
-            pipmodule,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-
-        stdout, stderr = await pipc.communicate()
-        pipout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        if pipout:
-            if len(pipout) > 4096:
-                await pip.edit("`Output too large, sending as file`")
-                with open("output.txt", "w+") as file:
-                    file.write(pipout)
-                await pip.client.send_file(
-                    pip.chat_id,
-                    "output.txt",
-                    reply_to=pip.id,
-                )
-                remove("output.txt")
-                return
-            await pip.edit(
-                "**Query: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Result: **\n`"
-                f"{pipout}"
-                "`"
-            )
-        else:
-            await pip.edit(
-                "**Query: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Result: **\n`No Result Returned/False`"
-            )
-    else:
-        await pip.edit("`Use .help pip to see an example`")
-
-
-@register(outgoing=True, pattern=r"^\.(alive|on)$")
+@borg.on(admin_cmd(outgoing=True, pattern="alive$"))
 async def amireallyalive(alive):
-    """For .alive command, check if the bot is running."""
-    uptime = await get_readable_time((time.time() - StartTime))
-    output = (
-        "**Wolverine is alive🦊**\n"
-        f"`•••••••••••••••••••••••••••••••••••••`\n"
-        
-        f"🧒 `𝙐𝙨𝙚𝙧            :  {DEFAULTUSER}`\n"
-        
-        f"🔥 `𝙋𝙮𝙩𝙝𝙤𝙣 𝙑𝙚𝙧𝙨𝙞𝙤𝙣   : v{python_version()}`\n"
-        
-        f"⚙️ `𝙏𝙚𝙡𝙚𝙩𝙝𝙤𝙣 𝙑𝙚𝙧𝙨𝙞𝙤𝙣  : v{version.__version__}`\n"
-        
-        f"🐺 `𝙒𝙤𝙡𝙫𝙚𝙧𝙞𝙣𝙚        : v{PROJECTDILS_VERSION}`\n"
-        
-        f"⛄ `𝙇𝙤𝙖𝙙𝙚𝙙 𝙈𝙤𝙙𝙪𝙡𝙚𝙨  : {len(modules)}`\n"
-        
-        f"📟 `𝘽𝙤𝙩 𝙎𝙥𝙚𝙚𝙙       : {uptime} `\n"
-        
-        f"`•••••••••••••••••••••••••••••••••••••`\n"
-        
-        f"🖥️**𝙒𝙤𝙡𝙑𝙚𝙧𝙞𝙉𝙚        : [Deploy](https://github.com/ShadoWClub/WolverineUserbot)**\n"
-    )
-    if ALIVE_LOGO:
-        try:
-            logo = ALIVE_LOGO
-            await alive.delete()
-            msg = await bot.send_file(alive.chat_id, logo, caption=output)
-        except MediaEmptyError:
-            msg = await alive.edit(
-                output + "\n\n *`The provided logo is invalid."
-                "\nMake sure the link is directed to the logo picture`"
-            )
-    else:
-        msg = await alive.edit(output)
-    await asyncio.sleep(45)
-    try:
-        await msg.delete()
-    except BaseException:
+    if alive.fwd_from:
         return
+    reply_to_id = alive.message
+    uptime = await wolfdef.get_readable_time((time.time() - StartTime))
+    if alive.reply_to_msg_id:
+        reply_to_id = await alive.get_reply_message()
+    if WOLF_IMG:
+         wolf_caption  = f"**Wolf User Bot 🐺 Running Fine**\n\n"
+         wolf_caption += f"**∂αтαвαѕє ѕтαтυѕ: (っ◔◡◔)っ ♥ Databases functioning normally!\n**"   
+         wolf_caption += f"➯ 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐯𝐞𝐫𝐬𝐢𝐨𝐧 : `{version.__version__}\n`"
+         wolf_caption += f"➯ 𝐏𝐞𝐫𝐬𝐨𝐧𝐚𝐥 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : `{wolfversion}`\n"
+         wolf_caption += f"➯ 𝐏𝐲𝐭𝐡𝐨𝐧 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : `{python_version()}\n\n`"
+         wolf_caption += f"**𝐈'𝐦 𝐡𝐞𝐫𝐞 𝐭𝐨 𝐡𝐞𝐥𝐩 𝐲𝐨𝐮, 𝐦𝐲 𝐦𝐚𝐬𝐭𝐞𝐫!\n**🐺"
+         wolf_caption += f"➯ My Master: {DEFAULTUSER}\n"
+         wolf_caption += f"➯ uptime : `{uptime}\n"
+         await borg.send_file(alive.chat_id, WOLF_IMG, caption=wolf_caption, reply_to=reply_to_id)
+         await alive.delete()
+    else:
+        await alive.edit(f"**Wolf User Bot 🐺 Running Fine**\n\n"
+                         "**∂αтαвαѕє ѕтαтυѕ: (っ◔◡◔)っ ♥ Databases functioning normally!\n**" 
+                         f"➯ 𝐓𝐞𝐥𝐞𝐭𝐡𝐨𝐧 𝐯𝐞𝐫𝐬𝐢𝐨𝐧 : `{version.__version__}\n`"
+                         f"➯ 𝐏𝐞𝐫𝐬𝐨𝐧𝐚𝐥 𝐀𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐭 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : `{wolfversion}`\n"
+                         f"➯ 𝐏𝐲𝐭𝐡𝐨𝐧 𝐕𝐞𝐫𝐬𝐢𝐨𝐧 : `{python_version()}\n\n`"
+                         "**𝐈'𝐦 𝐡𝐞𝐫𝐞 𝐭𝐨 𝐡𝐞𝐥𝐩 𝐲𝐨𝐮, 𝐦𝐲 𝐦𝐚𝐬𝐭𝐞𝐫!\n**🐺"
+                         f"➯ My Master: {DEFAULTUSER}\n"
+                         f"➯ uptime : `{uptime}\n`"
+                        )    
 
+@borg.on(sudo_cmd(pattern="sudo", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    uptime = await wolfdef.get_readable_time((time.time() - StartTime))
+    await event.reply(" SUDO COMMANDS ARE WORKING PERFECTLY \n\n"
+                      f"➯ Telethon version: {version.__version__}\n"
+                      f"➯ Python: {python_version()}\n"
+                      f"➯ My peru owner: {DEFAULTUSER}\n"
+                      f"**uptime :** `{uptime}\n`"
+                      #"Deploy this userbot Now"
+                     )
 
-@register(outgoing=True, pattern="^.aliveu")
-async def amireallyaliveuser(username):
-    """For .aliveu command, change the username in the .alive command."""
-    message = username.text
-    output = ".aliveu [new user without brackets] nor can it be empty"
-    if message != ".aliveu" and message[7:8] == " ":
-        newuser = message[8:]
-        global DEFAULTUSER
-        DEFAULTUSER = newuser
-        output = "Successfully changed user to " + newuser + "!"
-    await username.edit("`" f"{output}" "`")
-
-
-@register(outgoing=True, pattern=r"^\.resetalive$")
-async def amireallyalivereset(ureset):
-    """For .resetalive command, reset the username in the .alive command."""
-    global DEFAULTUSER
-    DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-    await ureset.edit("`" "Successfully reset user for alive!" "`")
-
-
-CMD_HELP.update(
-    {
-        "sysd": ">`.sysd`"
-        "\nUsage: Shows system information using neofetch."
-        "\n\n>`.spc`"
-        "\nUsage: Show system specification.",
-        "botver": ">`.botver`" "\nUsage: Shows the userbot version.",
-        "pip": ">`.pip <module(s)>`" "\nUsage: Does a search of pip modules(s).",
-        "alive": ">`.alive | .on`"
-        "\nUsage: Type .alive/on to see wether your bot is working or not."
-        "\n\n>`.aliveu <text>`"
-        "\nUsage: Changes the 'user' in alive to the text you want."
-        "\n\n>`.resetalive`"
-        "\nUsage: Resets the user to default.",
-    }
-)
+@borg.on(admin_cmd(pattern="wolf$"))
+async def _(event):
+    await event.delete() 
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    with open("temp.png", "wb") as f:
+        f.write(requests.get(nekos.wolf()).content)
+    img = Image.open("temp.png")
+    img.save("temp.webp", "webp")
+    img.seek(0)
+    await bot.send_file(event.chat_id , open("temp.webp", "rb"),reply_to=reply_to_id) 
+    
+CMD_HELP.update({"alive": "`.alive` :\
+      \n**USAGE:** Type .alive to see wether your bot is working or not.\
+      \n\n`.wolf`\
+      \n**USAGE : **Random wolf stickers"
+}) 
